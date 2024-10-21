2aa11a0e1e4e2c9b2b39fe810b53a2c6994e88ac
Areen Chakraborty <62027600+04ac@users.noreply.github.com>
2024-10-21 12:06:05 +0530

    Get new ui and logc working

--- app/src/main/java/com/areen/fixmystreet/ClickPicActivity.kt
+++ app/src/main/java/com/areen/fixmystreet/ClickPicActivity.kt
@@ -3,18 +3,24 @@ package com.areen.fixmystreet
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.location.Geocoder
 import android.location.Location
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -24,18 +30,45 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class ClickPicActivity : AppCompatActivity() {
 
+    private lateinit var clickedImgIV: ImageView
+    private lateinit var addressTV: TextView
     private var currLat: Double? = null
     private var currLng: Double? = null
     private var currAddress: String? = null
+    private var currentPhotoPath: String? = null
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_click_pic)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
+        clickedImgIV = findViewById(R.id.clickedImageIV)
+        addressTV = findViewById(R.id.addressTV)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        currentPhotoPath = intent.getStringExtra("currentPhotoPath")
+        currLat = intent.getDoubleExtra("currLat", 90000.0)
+        currLng = intent.getDoubleExtra("currLng", 90000.0)
+        currAddress = intent.getStringExtra("currAddress")
+
+        if (currLat == 90000.0) currLat = null
+        if (currLng == 90000.0) currLng = null
+
+        currentPhotoPath?.let {
+            setPic(it, clickedImgIV)
+        }
+
+        currLat?.let {
+            displayLocation()
+        }
 
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
@@ -49,7 +82,6 @@ class MainActivity : AppCompatActivity() {
             requestPermissionLauncher.launch(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION))
         } else {
             // Permission already granted from before
-            getLocation()
             dispatchTakePictureIntent()
         }
     }
@@ -57,7 +89,6 @@ class MainActivity : AppCompatActivity() {
     private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
         if (permissions[android.Manifest.permission.ACCESS_FINE_LOCATION] == true || permissions[android.Manifest.permission.ACCESS_COARSE_LOCATION] == true) {
             // Permission granted
-            getLocation()
             dispatchTakePictureIntent()
         } else {
             // Permission denied
@@ -65,17 +96,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // LOCATION STUFF
+    private fun getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                location?.let {
+                    currLat = it.latitude
+                    currLng = it.longitude
+                    getAddress(it.latitude, it.longitude)
+                }
+            }
+        }
+    }
+
+    private fun displayLocation() {
+        addressTV.text = currAddress
+        Toast.makeText(this, "Latitude: ${currLat}\nLongitude: ${currLng}", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getAddress(latitude: Double, longitude: Double) {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+        val address = addresses!![0].getAddressLine(0)
+        currAddress = address
+    }
+
+    // Camera Stuff -----------------------------------------------------------
+
     private val startCameraActivityForResult = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result: ActivityResult ->
         if (result.resultCode == Activity.RESULT_OK) {
-            val intent = Intent(this, ClickPicActivity::class.java).apply {
-                putExtra("currentPhotoPath", currentPhotoPath)
-                putExtra("currLat", currLat)
-                putExtra("currLng", currLng)
-                putExtra("currAddress", currAddress)
-            }
-            startActivity(intent)
+            getLocation()
+            displayLocation()
         }
     }
 
@@ -105,8 +159,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private var currentPhotoPath: String? = null
-
     @Throws(IOException::class)
     private fun createImageFile(): File {
         // Create an image file name
@@ -126,25 +178,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // LOCATION STUFF
-    private fun getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                location?.let {
-                    currLat = it.latitude
-                    currLng = it.longitude
-                    getAddress(it.latitude, it.longitude)
-                }
-            }
+    private fun setPic(photoPath: String, imageView: ImageView) {
+        // Get the dimensions of the View
+        val targetW: Int = imageView.width
+        val targetH: Int = imageView.height
+
+        val bmOptions = BitmapFactory.Options().apply {
+            // Get the dimensions of the bitmap
+            inJustDecodeBounds = true
+
+            BitmapFactory.decodeFile(photoPath)
+
+            val photoW: Int = outWidth
+            val photoH: Int = outHeight
+
+            // Determine how much to scale down the image
+//            val scaleFactor: Int = Math.max(1, Math.min(photoW / targetW, photoH / targetH))
+
+            // Decode the image file into a Bitmap sized to fill the View
+            inJustDecodeBounds = false
+//            inSampleSize = scaleFactor
+            inPurgeable = true
+        }
+
+        BitmapFactory.decodeFile(photoPath, bmOptions)?.also { bitmap ->
+            imageView.setImageBitmap(bitmap)
         }
     }
-
-    private fun getAddress(latitude: Double, longitude: Double) {
-        val geocoder = Geocoder(this, Locale.getDefault())
-        val addresses = geocoder.getFromLocation(latitude, longitude, 1)
-        val address = addresses!![0].getAddressLine(0)
-        currAddress = address
-    }
-
 }
